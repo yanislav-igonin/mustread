4fd63624557ff24e3b6488a019f45de8e8f35a2d
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-27 22:11:28 +0300

    update mustreads list

--- mustreads.ts
+++ mustreads.ts
@@ -1,15 +1,17 @@
-type Mustread = { title: string; url: string; tags: string[]; };
+type Mustread = { title: string; description: string; url: string; tags: string[]; };
 
 export const mustreads: Mustread[] = [
   {
-    title: 'React: A JavaScript library for building user interfaces',
-    url: 'https://reactjs.org/',
-    tags: ['react', 'specifications'],
+    title: 'Painless Functional Specifications',
+    description: 'A guide to writing functional specifications that are easy to understand and implement.',
+    url: 'https://www.joelonsoftware.com/2000/10/02/painless-functional-specifications-part-1-why-bother/',
+    tags: ['specifications'],
   },
   {
-    title: 'React: A JavaScript library for building user interfaces',
+    title: 'React Docs',
+    description: 'The official React documentation.',
     url: 'https://reactjs.org/',
-    tags: ['react', 'specifications'],
+    tags: ['react', 'documentation'],
   },
 ];
 
