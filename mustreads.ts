d1cd235ba1799ffe281eddca8cabdf13ec04c1d3
Yanislav Igonin <yanislavdabest@gmail.com>
2022-11-26 13:19:16 +0200

    add new mustreads

--- mustreads.ts
+++ mustreads.ts
@@ -7,6 +7,18 @@ export const mustreads: Mustread[] = [
     url: 'https://www.joelonsoftware.com/2000/10/02/painless-functional-specifications-part-1-why-bother/',
     tags: ['specifications'],
   },
+  {
+    title: 'Волчистость в IT',
+    description: 'В этой статьей ты узнаешь обо всех минусах осторожно расчетливого подхода к деланию дел. Мы поговорим о влиянии мемасов про волков на твой жизненный успех. И введем баззворд волчистость, ты же любишь щегольнуть новым словом?',
+    url: 'https://medium.com/@M0rtyMerr/%D0%B2%D0%BE%D0%BB%D1%87%D0%B8%D1%81%D1%82%D0%BE%D1%81%D1%82%D1%8C-%D0%B2-it-7767f5a07dbc',
+    tags: ['psychology'],
+  },
+  {
+    title: 'Ten Rules for Negotiating a Job Offer',
+    description: 'A 2-article seriel about negotiating. Want a good offer? Negotiate! But how? Here are 10 rules to help you get the best deal.',
+    url: 'https://haseebq.com/my-ten-rules-for-negotiating-a-job-offer/',
+    tags: ['psychology'],
+  },
 ];
 
 // Only unique tags.
